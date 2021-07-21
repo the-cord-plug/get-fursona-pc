8f27ac7dc96fbdf23ed2914aaa824472870cada0
jai 3 <47060357+jdadonut@users.noreply.github.com>
2021-07-21 08:27:48 -0400

    fixed thing

--- index.js
+++ index.js
@@ -1,10 +1,43 @@
-const { Plugin } = require('powercord/entities');
-
-module.exports = class Template extends Plugin {
-    startPlugin() {
-        return;
-    }
-    pluginWillUnload() {
-        return;
-    }
+const { Plugin } = require("powercord/entities");
+const { get } = require("powercord/http");
+module.exports = class GetFursona extends Plugin {
+  startPlugin() {
+    powercord.api.commands.registerCommand({
+      name: "fursona",
+      description: "Get a Fursona",
+      usage: "{c}fursona < random | code (number 1-99999) >",
+      execute: (args) => {
+        switch (args[0]) {
+          case "random":
+            return {
+              send: false,
+              result:
+                "https://thisfursonadoesnotexist.com/v2/jpgs-2x/seed" +
+                String(Math.random()).substring(2, 7) +
+                ".jpg",
+            };
+            break;
+          case "code":
+            return {
+              send: false,
+              result:
+                "https://thisfursonadoesnotexist.com/v2/jpgs-2x/seed" +
+                args[1] +
+                ".jpg",
+            };
+            break;
+          default:
+            return {
+              send: false,
+              result:
+                "That's not a valid subcommand.\nUsage: {c}fursona < random | code (number 1-99999) >",
+            };
+            break;
+        }
+      },
+    });
+  }
+  pluginWillUnload() {
+    powercord.api.commands.unregisterCommand("fursona");
+  }
 };
