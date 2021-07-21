88af7909f448e6799e643ab0700b018c198d8ed0
jai 3 <47060357+jdadonut@users.noreply.github.com>
2021-07-21 08:47:50 -0400

    fixed thing

--- index.js
+++ index.js
@@ -1,5 +1,8 @@
 const { Plugin } = require("powercord/entities");
 const { get } = require("powercord/http");
+const { getModule, channels } = require("powercord/webpack");
+const { createBotMessage } = getModule([ "createBotMessage" ], false)
+const { receiveMessage }   = getModule([ "receiveMessage" ], false)
 module.exports = class GetFursona extends Plugin {
   startPlugin() {
     powercord.api.commands.registerCommand({
@@ -9,34 +12,37 @@ module.exports = class GetFursona extends Plugin {
       executor: (args) => {
         switch (args[0]) {
           case "random":
-            return {
-              send: false,
-              result:
-                "https://thisfursonadoesnotexist.com/v2/jpgs-2x/seed" +
+          case "":
+                let uri = "https://thisfursonadoesnotexist.com/v2/jpgs-2x/seed" +
                 String(Math.random()).substring(2, 7) +
                 ".jpg",
-            };
             break;
           case "code":
-            return {
-              send: false,
-              result:
-                "https://thisfursonadoesnotexist.com/v2/jpgs-2x/seed" +
+                            let uri = "https://thisfursonadoesnotexist.com/v2/jpgs-2x/seed" +
                 args[1] +
                 ".jpg",
-            };
             break;
           default:
-            return {
-              send: false,
-              result:
-                "That's not a valid subcommand.\nUsage: {c}fursona < random | code (number 1-99999) >",
-            };
-            break;
+                return {send: false, result: "Invalid subcommand."};
         }
+
       },
     });
   }
+  sendBotMessage(media_uri) {
+      let m = createBotMessage(channels.getChannelId(), "");
+      m.embeds.push({
+          type: "image",
+          image: {
+              url: media_uri,
+              proxy_url: media_uri,
+              width: 1024,
+              height: 1024,
+          },
+          }
+      );
+  }
+
   pluginWillUnload() {
     powercord.api.commands.unregisterCommand("fursona");
   }
